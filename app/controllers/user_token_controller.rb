6d156a73c353f74f6881723bf84bd56125fcf343
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-11 12:59:37 +0100

    api improvement with anonymous data ready to be handled

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,2 +1,57 @@
 class UserTokenController < Knock::AuthTokenController
+
+  before_action :anonymous_fallback
+  before_action :authenticate
+
+  def create
+    render json: auth_token, status: :created
+  end
+
+  private
+
+  # this is a hook to the system
+  # we ensure there will be a user if the front client
+  # required an anonymous access
+  def anonymous_fallback
+    if params[:anonymous]
+      # TODO : here we build the whole anonymous user and
+      # reset the params as it was sent by him ?
+      binding.pry
+    end
+  end
+
+  def authenticate
+    unless entity.present? && entity.authenticate(auth_params[:password])
+      raise Knock.not_found_exception_class
+    end
+  end
+
+  def auth_token
+    if entity.respond_to? :to_token_payload
+      AuthToken.new payload: entity.to_token_payload
+    else
+      AuthToken.new payload: { sub: entity.id }
+    end
+  end
+
+  def entity
+    @entity ||=
+    if entity_class.respond_to? :from_token_request
+      entity_class.from_token_request request
+    else
+      entity_class.find_by email: auth_params[:email]
+    end
+  end
+
+  def entity_class
+    entity_name.constantize
+  end
+
+  def entity_name
+    self.class.name.scan(/\w+/).last.split('TokenController').first
+  end
+
+  def auth_params
+    params.require(:auth).permit :email, :password
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20171211033814) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "role"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
