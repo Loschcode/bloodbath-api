827907959a56613bad352659f34517f2987bc090
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-11 01:55:28 +0100

    api call improved

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,5 +1,5 @@
 class CurrenciesController < ApplicationController
-  attr_reader :currency, :base_value, :current_value
+  attr_reader :currency, :currency_state, :currency_tracking
 
   before_action :authenticate_user!
   before_action :set_currency
@@ -9,16 +9,16 @@ class CurrenciesController < ApplicationController
 
   def show
     @currency_tracking = tracking_handler.solve
+    @currency_state = currency_tracking.currency_state
 
-    base_value = @currency_tracking.base_price
-    current_value = @currency_tracking.currency_state.price
-
-    difference = (100 - (base_value / current_value) * 100).round(2)
+    # TODO : put the logic in the front-end
+    difference = (100 - (currency_tracking.base_price / currency_state.price) * 100).round(2)
     render json: {
-      name: currency.upcase,
-      base_value: base_value,
-      current_value: current_value,
-      difference: difference }
+      success: true,
+      currency_tracking: currency_tracking,
+      currency_state: currency_state,
+      metadata: { difference: difference }
+    }
   end
 
   def destroy
