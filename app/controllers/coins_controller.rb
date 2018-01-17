55f911a3defb36b6e059770007b50bd065603e54
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-18 00:54:39 +0100

    minor refactoring hash fetching

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -9,33 +9,38 @@ class CoinsController < ApplicationController
   end
 
   def favorite
-    @favorite_coins = current_user.user_market_coins.with_favorite.map(&:market_coin)
-    throw_success favorite_coins: top_coins
+    @market_coins = current_user.user_market_coins.with_favorite.map(&:market_coin)
+    throw_success favorite_coins: coins_hash(market_coins)
   end
 
   def top
     @market_coins = MarketCoin.order(market_cap: :desc).order(sort_order: :asc).limit(8)
-    top_coins = market_coins.reduce([]) do |acc, market_coin|
-      acc << {market_coin: market_coin, user_market_coin: TrackingHandler.new(user: current_user, market_coin: market_coin).solve}
-    end
+    top_coins = coins_hash(market_coins)
     throw_success top_coins: top_coins
   end
 
   def search
     query = params[:query]
     @market_coins = MarketCoin.search(query).order(market_cap: :desc).order(sort_order: :asc).limit(4)
-    result_coins = market_coins.reduce([]) do |acc, market_coin|
-      acc << {market_coin: market_coin, user_market_coin: TrackingHandler.new(user: current_user, market_coin: market_coin).solve}
-    end
+    result_coins = coins_hash(market_coins)
     throw_success result_coins: result_coins
   end
 
   def show
-    @user_market_coin = TrackingHandler.new(user: current_user, market_coin: market_coin).solve
+    @user_market_coin = current_user.user_market_coins.where(market_coin: market_coin).first
     throw_success user_market_coin: user_market_coin, market_coin: market_coin
   end
 
   private
+
+  def coins_hash(market_coins)
+    market_coins.reduce([]) do |acc, market_coin|
+      acc << {
+        market_coin: market_coin,
+        user_market_coin: current_user.user_market_coins.where(market_coin: market_coin).first
+      }
+    end
+  end
 
   def set_currency
     @currency = params[:id].upcase
