c892ec8b9b9e09a2e61adf11a14aa463f32139e8
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-30 01:09:35 +0100

    currency controller improved

--- app/controllers/base_currencies_controller.rb
+++ app/controllers/base_currencies_controller.rb
@@ -3,11 +3,21 @@ class BaseCurrenciesController < ApplicationController
 
   before_action :authenticated?
 
+  before_action :set_base_currency, only: [:show]
+
   def index
     @base_currencies = BaseCurrency.all
     throw_success base_currencies: base_currencies
   end
 
+  def show
+    throw_success base_currency: base_currency
+  end
+
   private
+
+  def set_base_currency
+    @base_currency = BaseCurrency.find(params[:id])
+  end
 
 end
