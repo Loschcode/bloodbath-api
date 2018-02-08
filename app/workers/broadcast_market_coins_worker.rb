a940f41a96269a29991cab93e852062ea83e2090
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-02-08 22:22:00 +0100

    bugfix worker

--- app/workers/broadcast_market_coins_worker.rb
+++ app/workers/broadcast_market_coins_worker.rb
@@ -2,6 +2,6 @@ class BroadcastMarketCoinsWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    Tasks::Cron::BroadcastMarketCoins.new.perform
+    ::Tasks::Cron::BroadcastMarketCoins.new.perform
   end
 end
