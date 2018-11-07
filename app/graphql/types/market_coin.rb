141ac4350a01bf16a2eddc80a697b5baaf695cec
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-11-07 18:06:16 -0300

    light bugfix on types

--- app/graphql/types/market_coin.rb
+++ app/graphql/types/market_coin.rb
@@ -12,8 +12,8 @@ module Types
     field :logo_url, String, null: false
     field :algorithm, String, null: false
     field :proof_type, String, null: false
-    field :market_cap, String, null: false
 
+    field :market_cap, Float, null: false
     field :price, Float, null: false
     field :day_open, Float, null: false
     field :day_high, Float, null: false
@@ -24,7 +24,7 @@ module Types
     # this is not good for now
     field :all_time_high, Float, null: false
 
-    field :rank, Integer, null: false
+    field :rank, Integer, null: true
 
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
